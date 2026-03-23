4c3ab5e3f2ef1c219b198f2183ccc89c81d63fa7
Ziming Gao <zimingg@andrew.cmu.edu>
2026-03-23 13:48:00 -0400

    Break up eval shell into two shells on same H100 node

---
+++ eval_qwen_hosting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This shall be run on a H100-80 type node on PSC.
+# Same machine hosts Qwen AND the evaluation code.
+export OPENAI_BASE_URL=http://localhost:8000/v1
+export EVAL_OPENAI_BASE_URL=https://ai-gateway.andrew.cmu.edu
+
+
+echo "Setting up SGLANG and hosting QwenV2.5..."
+module load cuda
+conda activate sglang_env
+
+python -m sglang.launch_server \
+    --model-path Qwen/Qwen2.5-VL-7B-Instruct \
+    --chat-template qwen2-vl \
+    --port 8000 \
+    --host 0.0.0.0 \
+    --mem-fraction-static 0.5 \
+    --context-length 8192 \
+    --max-running-requests 10 \
+
--- eval_qwen_vwa.sh
+++ eval_qwen_vwa.sh
@@ -6,22 +6,10 @@ export OPENAI_BASE_URL=http://localhost:8000/v1
 export EVAL_OPENAI_BASE_URL=https://ai-gateway.andrew.cmu.edu
 
 echo "Sourcing conda environment..."
-export PATH="/ocean/projects/cis260044p/shared/miniconda3/bin:$PATH"
-. /ocean/projects/cis260044p/shared/miniconda3/etc/profile.d/conda.sh
 
-echo "Setting up SGLANG and hosting QwenV2.5..."
 module load cuda
-conda activate sglang_env
 
-python -m sglang.launch_server \
-    --model-path Qwen/Qwen2.5-VL-7B-Instruct \
-    --chat-template qwen2-vl \
-    --port 8000 \
-    --host 0.0.0.0 \
-    --mem-fraction-static 0.7 \
-    --context-length 8192 \
-    --max-running-requests 10 \
-    > /dev/null 2>&1
+
 
 # Run VWA evaluation code
 echo "Setting up VWA..."
